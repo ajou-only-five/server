ee16aa6bdfcb7db9952f70dab3370ddb714ea223
NAJOO0 <sklwndud@ajou.ac.kr>
2022-11-29 23:53:20 +0900

    njy commit 1

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const db_helper = require('./db/db_helper');
+//const db_helper = require('./db/db_helper');
 const indexRouter = require('./routers/index');
 const usersRouter = require('./routers/users');
-
-const app = express();
+const todoItemRouter = require('./routers/todoItem');
+const todoItemDoneRouter = require('./routers/todoItemDone');
+const followRouter = require('./routers/follow');
+const searchRouter = require('./routers/search');
+const myInfoRouter = require('./routers/myInfo');
+const todoTitleRouter = require('./routers/todoTitle')
+const authRouter = require('./routers/auth')
+const validRouter = require('./routers/valid')
+const app = express()
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,5 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/todoItem',todoItemRouter)
+app.use('/todoItemDone',todoItemDoneRouter)
+app.use('/follow',followRouter)
+app.use('/myInfo',myInfoRouter)
+app.use('/search',searchRouter)
+app.use('/todoTitle',todoTitleRouter)
+app.use('/valid',validRouter)
+app.use('/auth',authRouter)
+/*
+*/
 module.exports = app;
--- routers/valid.js
+++ routers/valid.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users');
+  res.send('valid');
+  next()
 });
 
 module.exports = router;
