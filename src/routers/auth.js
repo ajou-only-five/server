3bb92051b7359c90c7a18187ec3b61a39a79d975
NAJOOYEONG <sklwndud@ajou.ac.kr>
2022-12-06 02:29:29 +0900

    update auth router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,7 @@
-var express = require("express");
+import express from "express";
 import { oracleDbHelper } from "../db/index.js";
+import session from "express-session";
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -9,60 +11,42 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/sign-up", async function (req, res, next) {
-  try {
-    const result = await oracleDbHelper.insert({
-      table: "Users",
-      columns: ["test", "test"],
-      data: ["test", "test"],
-    });
-    console.log(result);
-
-    if (result.code === 200) {
-      return res.status(200).send("user created succeed");
-    }
-
-    res.status(400).send("client error");
-    // res.status(500).send('server error');
-  } catch (e) {
+  const data = {
+    account: req.body.accountName,
+    password: req.body.password,
+    nickname: req.body.nickname,
+    profile: "default",
+    disclosure: 0,
+  };
+  let result = await UserServices.createUser(data);
+  if (result.status) {
+    res.status(200).send("signed success");
+  } else {
     res.status(500).send("server error");
   }
+  return result;
 });
 router.post("/login", async function (req, res, next) {
-  try {
-    const result = await oracleDbHelper.insert({
-      table: "Users",
-      columns: ["test", "test"],
-      data: ["test", "test"],
-    });
-    console.log(result);
-
-    if (result.code === 200) {
-      return res.status(200).send("user created succeed");
-    }
-
-    res.status(400).send("client error");
-    // res.status(500).send('server error');
-  } catch (e) {
-    res.status(500).send("server error");
+  let result = await UserServices.findUserByAccount(req.body.accountName);
+  if (result.status) {
+    //정보가 있으면 세션에 저장.
+    req.session.account = req.body.accountName;
+    req.session.password = req.body.password;
+    return res.status(200).json("true");
+  } else {
+    return res.status(500).json("일치하는 account가 없습니다.");
   }
 });
 router.post("/logout", async function (req, res, next) {
-  try {
-    const result = await oracleDbHelper.insert({
-      table: "Users",
-      columns: ["test", "test"],
-      data: ["test", "test"],
+  if (req.session.account) {
+    //세션 정보가 있으면 세션을 삭제해줌
+    await req.session.destroy(function (err) {
+      if (err) {
+        return res.status(500).send(false);
+      } else {
+        return res.status(200).send(true);
+      }
     });
-    console.log(result);
-
-    if (result.code === 200) {
-      return res.status(200).send("user created succeed");
-    }
-
-    res.status(400).send("client error");
-    // res.status(500).send('server error');
-  } catch (e) {
-    res.status(500).send("server error");
   }
 });
-module.exports = router;
+export { router as authRouter };
