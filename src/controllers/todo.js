f51790cdf1d239f3ebc49e189b99a67f48c192ef
Long9725 <develop9725@gmail.com>
2022-12-08 16:21:16 +0000

    <FEAT> update and delete todo item router

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -43,10 +43,6 @@ export default Object.freeze({
         }
     },
     createTodoItem: async (req, res, next) => {
-        // * @property { Number } titleId - data.titleId, todo title index
-        // * @property { String } content - data.content, todo 내용
-        // * @property { Date? } startAt - data.startAt, todo 시작 시간
-        // * @property { Date? } endAt - data.endAt, todo 종료 시간
         const data = {
             ...req.body
         };
@@ -163,5 +159,153 @@ export default Object.freeze({
         } catch (e) {
             return res.status(500).send("Server error");
         }
-    }
+    },
+    updateTodoTitle: async (req, res, next) => {
+        const data = {
+            ...req.body
+        };
+
+        if (data.titleId === undefined) {
+            return res.status(400).send("titleId must be required.");
+        }
+        if (!TypeChecker.isInteger(data.titleId)) {
+            return res.status(400).send("titleId must be integer.");
+        }
+        if (data.title === undefined) {
+            return res.status(400).send("title must be required.");
+        }
+        if (!TypeChecker.isString(data.title)) {
+            return res.status(400).send("title must be string.");
+        }
+        if (data.color === undefined) {
+            return res.status(400).send("color must be required.");
+        }
+        if (!TypeChecker.isString(data.color)) {
+            return res.status(400).send("color must be string.");
+        }
+
+        data.titleId = parseInt(data.titleId);
+
+        try {
+            const result = await TodoServices.updateTodoTitleByTitleId(data);
+
+            if (result.status) {
+                return res.status(200).send(result.data);
+            }
+
+            return res.status(500).send("Server error.");
+        } catch (e) {
+            return res.status(500).send("Server error");
+        }
+    },
+    updateTodoItem: async (req, res, next) => {
+        const data = {
+            ...req.body
+        };
+
+        if (data.itemId === undefined) {
+            return res.status(400).send("titleId must be required.");
+        }
+        if (!TypeChecker.isInteger(data.titleId)) {
+            return res.status(400).send("titleId must be integer.");
+        }
+        if (data.content === undefined) {
+            return res.status(400).send("contenst must be required.");
+        }
+        if (!TypeChecker.isString(data.content)) {
+            return res.status(400).send("content must be string.");
+        }
+        if (data.isChecked === undefined) {
+            return res.status(400).send("isChecked must be required.");
+        }
+        if (data.isChecked === 0 || data.isChecked === 1) {
+            return res.status(400).send("isChecked must be 0 or 1.");
+        }
+
+        if (data.startAt !== undefined) {
+            const startAt = new Date(data.startAt);
+
+            if (startAt.toString().includes('Invalid Date')) {
+                return res.status(400).send("startAt must be date.");
+            }
+
+            data.startAt = startAt;
+        }
+
+        if (data.endAt !== undefined) {
+            const endAt = new Date(data.endAt);
+
+            if (endAt.toString().includes('Invalid Date')) {
+                return res.status(400).send("endAt must be date.");
+            }
+
+            data.endAt = endAt;
+        }
+
+        data.itemId = parseInt(data.isChecked);
+
+        try {
+            const result = await TodoServices.updateTodoItemByItemId(data);
+
+            if (result.status) {
+                return res.status(200).send(result.data);
+            }
+
+            return res.status(500).send("Server error.");
+        } catch (e) {
+            return res.status(500).send("Server error");
+        }
+    },
+    deleteTodoTitle: async (req, res, next) => {
+        const data = {
+            ...req.body
+        };
+
+        if (data.titleId === undefined) {
+            return res.status(400).send("titleId must be required.");
+        }
+        if (!TypeChecker.isInteger(data.titleId)) {
+            return res.status(400).send("titleId must be integer.");
+        }
+
+        data.titleId = parseInt(data.titleId);
+
+        try {
+            const result = await TodoServices.deleteTodoTitle(data);
+
+            if (result.status) {
+                return res.status(200).send(result.data);
+            }
+
+            return res.status(500).send("Server error.");
+        } catch (e) {
+            return res.status(500).send("Server error");
+        }
+    },
+    deleteTodoItem: async (req, res, next) => {
+        const data = {
+            ...req.body
+        };
+
+        if (data.itemId === undefined) {
+            return res.status(400).send("itemId must be required.");
+        }
+        if (!TypeChecker.isInteger(data.itemId)) {
+            return res.status(400).send("itemId must be integer.");
+        }
+
+        data.itemId = parseInt(data.itemId);
+
+        try {
+            const result = await TodoServices.deleteTodoItem(data);
+
+            if (result.status) {
+                return res.status(200).send(result.data);
+            }
+
+            return res.status(500).send("Server error.");
+        } catch (e) {
+            return res.status(500).send("Server error");
+        }
+    },
 });
--- src/routers/todo.js
+++ src/routers/todo.js
@@ -4,9 +4,12 @@ import { TodoController } from '../controllers/index.js';
 
 const router = express.Router();
 
-router.get("/todoList", TodoController.searchTodoListInMonth);
+router.get('/search/todoList', TodoController.searchTodoListInMonth);
 
-router.post("/title", TodoController.createTodoTitle);
-router.post("/item", TodoController.createTodoItem);
+router.post('/title', TodoController.createTodoTitle);
+router.post('/item', TodoController.createTodoItem);
+
+router.patch('/title',TodoController.updateTodoTitle);
+router.patch('/title',TodoController.updateTodoItem);
 
 export default router;
