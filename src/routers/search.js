5e5f880837760c4846f534beea1eed17b4619faf
NaJoo <sklwndud@ajou.ac.kr>
2022-12-09 01:42:51 +0000

    merge

--- src/routers/search.js
+++ src/routers/search.js
@@ -3,75 +3,9 @@ import UserServices from '../services/user.js';
 
 import { SearchController } from '../controllers/index.js';
 
-<<<<<<< HEAD
-/* GET users listing. */
-router.get("/", async function (req, res, next) {
-  try {
-    let data={
-      nickname:req.body.nickname
-    }
-    let result = await UserServices.searchUserByNickname(data)
-    if(result.status){
-      if(result.data.length===0){
-        return res.status(400).json({
-          message:"해당 nickname을 가진 user가 없습니다"
-        })
-      }
-      else{
-        return res.status(200).json({
-          message:"해당 nickname을 가진 user가 있습니다",
-          data:result.data
-        })
-      }
-    }
-    else{
-      return res.status(500).json({
-        message:"db error"
-      })
-    }
-  } catch (e){
-
-    res.status(500).json({message:"server error"});
-  }
-});
-router.get("/between",async function(req,res,next){
-  try{
-    let data={
-      nickname:req.body.nickname,
-      start:parseInt(req.body.start),
-      end:parseInt(req.body.end)
-    }
-    let result = await UserServices.searchUserByNicknameBetween(data)
-    if(result.status){
-      if(result.data.length===0){
-        return res.status(400).json({
-          message:"해당 nickname을 가진 유저가 없습니다"
-        })
-      }
-      else{
-        return res.status(200).json({
-          message:"해당 nickname을 가진 유저 중 "+data.start+" 부터 "+data.end+" 까지의 유저입니다",
-          data:result.data
-        })
-      }
-    }
-    else{
-      return res.status(500).json({
-        message:"db error"
-      })
-    }
-  }catch(err){
-    return res.status(500).json({
-      message:"server error"
-    })
-  }
-})
-export {router as searchRouter}
-=======
 const router = express.Router();
 
 router.get('/friend', SearchController.searchFriend);
 router.get('/friendRequsted', SearchController.searchFriendRequsted);
 
 export default router;
->>>>>>> d8bc733df980e265f4e8b954c48f9e81c1553094
