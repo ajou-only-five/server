ac9a384ab3562b9d242275861a37ca69a6db9cb3
Long9725 <develop9725@gmail.com>
2022-12-10 04:25:50 +0000

    <FEAT> searchNotFriend to search services

--- src/query/search.js
+++ src/query/search.js
@@ -283,11 +283,11 @@ FROM (SELECT id, account, nickname, profile, disclosure
     )) WHERE NUM BETWEEN :start AND :end
     `,
     /**
-* @param { Number } userId 본인
-* @param { String } nickname 검색하려는 닉네임
-* @param { Number } start 시작 범위
-* @param { Number } end 종료 범위
-*/
+    * @param { Number } userId 본인
+    * @param { String } nickname 검색하려는 닉네임
+    * @param { Number } start 시작 범위
+    * @param { Number } end 종료 범위
+    */
     searchNotFriendAndNotRequestedByUserIdAndNicknameBetween: (nickname) => `
         SELECT id, account, nickname, profile, disclosure
         FROM (
@@ -319,5 +319,150 @@ FROM (SELECT id, account, nickname, profile, disclosure
                 ORDER BY nickname ASC, id ASC
             ) WHERE nickname LIKE '%${nickname}%'
         ) WHERE NUM BETWEEN :start AND :end
-        `
+        `,
+        /**
+        * @param { Number } userId 본인
+        */
+        searchNotFriendByUserId: `
+        SELECT user_id, account, nickname, profile, disclosure, relation
+        FROM (
+            SELECT id as user_id, account, nickname, profile, U.disclosure,  
+            CASE WHEN U.id IN (
+                SELECT follower_id
+                FROM FRIEND_REQUEST
+                WHERE followee_id = :user_id
+            ) THEN 0 
+            WHEN id IN (
+                SELECT followee_id
+                FROM FRIEND_REQUEST
+                WHERE follower_id = :user_id
+            ) THEN 1
+            WHEN (
+                id <> :user_id
+                AND id NOT IN (
+                    SELECT user_id_2
+                    FROM FRIEND
+                    WHERE user_id_1 = :user_id
+                )
+                AND id NOT IN (
+                    SELECT user_id_1
+                    FROM FRIEND
+                    WHERE user_id_2 = :user_id
+                )
+                AND id NOT IN (
+                    SELECT follower_id
+                    FROM FRIEND_REQUEST
+                    WHERE followee_id = :user_id
+                )
+                AND id NOT IN (
+                    SELECT followee_id
+                    FROM FRIEND_REQUEST
+                    WHERE follower_id = :user_id
+                )
+            ) THEN 2
+            ELSE 3 END as relation
+            FROM USERS
+            ORDER BY relation ASC
+        ) 
+        WHERE relation <> 3
+        `,
+        /**
+        * @param { Number } userId 본인
+        * @param { String } nickname 검색하려는 닉네임
+        */
+         searchNotFriendByUserIdAndNickname: (nickname) => `
+         SELECT user_id, account, nickname, profile, disclosure, relation
+         FROM (
+             SELECT id as user_id, account, nickname, profile, disclosure,  
+             CASE WHEN id IN (
+                 SELECT follower_id
+                 FROM FRIEND_REQUEST
+                 WHERE followee_id = :user_id
+             ) THEN 0 
+             WHEN id IN (
+                 SELECT followee_id
+                 FROM FRIEND_REQUEST
+                 WHERE follower_id = :user_id
+             ) THEN 1
+             WHEN (
+                 id <> :user_id
+                 AND U.id NOT IN (
+                     SELECT user_id_2
+                     FROM FRIEND
+                     WHERE user_id_1 = :user_id
+                 )
+                 AND id NOT IN (
+                     SELECT user_id_1
+                     FROM FRIEND
+                     WHERE user_id_2 = :user_id
+                 )
+                 AND id NOT IN (
+                     SELECT follower_id
+                     FROM FRIEND_REQUEST
+                     WHERE followee_id = :user_id
+                 )
+                 AND id NOT IN (
+                     SELECT followee_id
+                     FROM FRIEND_REQUEST
+                     WHERE follower_id = :user_id
+                 )
+             ) THEN 2
+             ELSE 3 END as relation
+             FROM USERS
+             WHERE nickname LIKE '%${nickname}%'
+             ORDER BY relation ASC
+         )
+         WHERE relation <> 3
+         `,
+         /**
+        * @param { Number } userId 본인
+        * @param { String } nickname 검색하려는 닉네임
+        * @param { Number } start 시작 범위
+        * @param { Number } end 종료 범위
+        */
+          searchNotFriendByUserIdAndNicknameBetweenStartAndEnd: (nickname) => `
+          SELECT user_id, account, nickname, profile, disclosure, relation
+          FROM (
+              SELECT id as user_id, account, nickname, profile, disclosure,  
+              CASE WHEN id IN (
+                  SELECT follower_id
+                  FROM FRIEND_REQUEST
+                  WHERE followee_id = :user_id
+              ) THEN 0 
+              WHEN id IN (
+                  SELECT followee_id
+                  FROM FRIEND_REQUEST
+                  WHERE follower_id = :user_id
+              ) THEN 1
+              WHEN (
+                  id <> :user_id
+                  AND U.id NOT IN (
+                      SELECT user_id_2
+                      FROM FRIEND
+                      WHERE user_id_1 = :user_id
+                  )
+                  AND id NOT IN (
+                      SELECT user_id_1
+                      FROM FRIEND
+                      WHERE user_id_2 = :user_id
+                  )
+                  AND id NOT IN (
+                      SELECT follower_id
+                      FROM FRIEND_REQUEST
+                      WHERE followee_id = :user_id
+                  )
+                  AND id NOT IN (
+                      SELECT followee_id
+                      FROM FRIEND_REQUEST
+                      WHERE follower_id = :user_id
+                  )
+              ) THEN 2
+              ELSE 3 END as relation
+              FROM USERS
+              WHERE nickname LIKE '%${nickname}%'
+              ORDER BY relation ASC
+          )
+          WHERE relation <> 3
+          AND NUM BETWEEN :start AND :end
+          `
 });
