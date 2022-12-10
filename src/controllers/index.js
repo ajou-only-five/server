6cc89c1d5086e2243a591773b36a9698d5c175b2
NaJoo <sklwndud@ajou.ac.kr>
2022-12-10 21:58:09 +0000

    add Authcontroller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,11 @@ import TodoController from './todo.js';
 import FriendRequestController from './friendRequest.js';
 import FriendController from './friend.js';
 import SearchController from './search.js';
-
+import AuthController from './auth.js';
 export {
     TodoController,
     FriendRequestController,
     FriendController,
-    SearchController
+    SearchController,
+    AuthController
 };
